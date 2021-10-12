637a914a24b162289d70b54e878236e44bc724cd
Luis Quinones <lpaandres2020@gmail.com>
2021-10-12 15:50:41 -0500

    Database creation, pending

--- src/db/db.py
+++ src/db/db.py
@@ -8,7 +8,12 @@ class DatabaseConnection:
             self.connection = sqlite3.connect("db/db.db")
         
         self.cursor = self.connection.cursor()
-        
+
+        """self.cursor.execute("CREATE TABLE citizen (identification INTEGER NOT NULL PRIMARY KEY)")
+        self.cursor.execute("CREATE TABLE candidates(ID VARCHAR NOT NULL PRIMARY KEY, fullname VARCHAR(50) NOT NULL PRIMARY KEY)")
+        self.cursor.execute("CREATE TABLE votes(citizen INTEGER NOT NULL, candidatesId VARCHAR)")"""
+        self.connection.commit()
+
 
 
 if __name__ == '__main__':
