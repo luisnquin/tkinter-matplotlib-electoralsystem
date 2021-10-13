b96a629f56fbd4531cf234bda3f409d4d8edb2e6
Luis Quinones <lpaandres2020@gmail.com>
2021-10-12 22:29:08 -0500

    Canvas graphic, icon, labels, comboboxes, requeriments and more, added

--- src/db/db.py
+++ src/db/db.py
@@ -2,6 +2,8 @@ import sqlite3
 
 class DatabaseConnection:
     def __init__(self):
+        self.dataToSent, self.receivedData = [None, None, None], [None, None, None]
+
         if __name__=='__main__':
             self.connection = sqlite3.connect("db.db")
         else:
@@ -9,12 +11,21 @@ class DatabaseConnection:
         
         self.cursor = self.connection.cursor()
 
-        """self.cursor.execute("CREATE TABLE citizen (identification INTEGER NOT NULL PRIMARY KEY)")
-        self.cursor.execute("CREATE TABLE candidates(ID VARCHAR NOT NULL PRIMARY KEY, fullname VARCHAR(50) NOT NULL PRIMARY KEY)")
-        self.cursor.execute("CREATE TABLE votes(citizen INTEGER NOT NULL, candidatesId VARCHAR)")"""
+    def insertOne(self):
+        self.cursor.execute("INSERT INTO votes VALUES(?, ?, ?)", self.receivedData)
         self.connection.commit()
 
+    def getData(self):
+        self.cursor.execute("SELECT * FROM votes")
+        self.dataToSent = self.cursor.fetchall()
+
+    def deleteData(self):
+        self.cursor.execute("DELETE FROM votes WHERE citizenId = ?")
+        self.connection.commit()
 
 
 if __name__ == '__main__':
     DatabaseConnection()
+
+
+"""self.cursor.execute("CREATE TABLE votes(citizenId INTEGER NOT NULL PRIMARY KEY, votePre VARCHAR(60) NOT NULL, voteVipre VARCHAR(60) NOT NULL)")"""
--- src/main.py
+++ src/main.py
@@ -1,8 +1,55 @@
+from tkinter.ttk import Combobox
 from tkinter import *
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from pandas import DataFrame
 
 class ElectoralSystem:
     def __init__(self):
-        pass
+        self.root = Tk()
+        self.root.title("Sistema electoral")
+        self.root.iconbitmap("assets/elec-sys.ico")
+        self.root.geometry("1200x700")
+
+        self.body()
+
+        self.root.mainloop()
+
+    def body(self):
+        Label(self.root, text="SISTEMA ELECTORAL", font=("Helvetica", 26)).place(x=50, y=50)
+        Label(self.root, text="Presidentes", font=("Helvetica", 18)).place(x=50, y=150)
+        presidentGraphic = Frame(bg="#949292", width=215, height=620)
+        presidentGraphic.place(x=50, y=200)
+
+        Data1 = {"Presidentes": ['A', 'B'], 'Elecciones': [50, 24]}
+        df1 = DataFrame(Data1, columns = ['Presidentes', 'Elecciones'])
+        df1 = df1[['Presidentes', 'Elecciones']].groupby('Presidentes').sum()
+
+        graphic1 = plt.Figure(figsize=(6, 5), dpi=50)
+        bars = graphic1.add_subplot(111)
+        bar1 = FigureCanvasTkAgg(graphic1, presidentGraphic)
+        bar1.get_tk_widget().pack(side=LEFT, fill=BOTH)
+        df1.plot(kind='bar', legend=True, ax=bars)
+        bars.set_title('Presidente')
+        
+        Label(self.root, text="Vicepresidentes", font=("Helvetica", 18)).place(x=50, y=450)
+        # FREE SPACE
+
+        Frame(self.root, width=2, height=500, bg="#000000").place(x=650, y=150)
+        Label(self.root, text="Ingrese su número de cédula: ", font=("Helvetica", 18)).place(x=700, y = 150)
+        Entry(self.root, width=18, font=("Helvetica", 25)).place(x=700, y=200)
+        Label(self.root, text="Presidente a elegir: ", font=("Helvetica", 18)).place(x=700, y = 350)
+        presidentCombox = Combobox(self.root, state="readonly", width=25, font=("Helvetica", 18))
+        presidentCombox["values"] = ["Guillermo Lasso", "Segundo Andres Arauz", "Cynthia Viteri"]
+        presidentCombox.place(x=700, y=400)
+
+        Label(self.root, text="Vicepresidente a elegir: ", font=("Helvetica", 18)).place(x=700, y = 550)
+        vicepresidentCombox = Combobox(self.root, state="readonly", width=25, font=("Helvetica", 18))
+        vicepresidentCombox["values"] = ["Doris Quiroz", "Ramiro Aguilar", "Jorge Glas"]
+        vicepresidentCombox.place(x=700, y=600)
+
 
 
 if __name__ == '__main__':
